5ed05474875ade37e1e31ddb388dc820c8a1472b
mcnallyj1037 <jmcnally@mcnallysoftware.com>
2021-12-09 20:47:03 -0500

    Code Cleanup

--- src/main/java/com/edwardjones/reminder/service/StickyNotesService.java
+++ src/main/java/com/edwardjones/reminder/service/StickyNotesService.java
@@ -19,16 +19,34 @@ public class StickyNotesService {
 	@Autowired
 	StickyNoteDao stickyNoteDao;
 
+	/**
+	 * Create a sticky note service method.
+	 * @param uniqueKey
+	 * @param title
+	 * @param description
+	 * @param reminderDate
+	 * @param phone
+	 * @param email
+	 */
 	public void createStickyNote(String uniqueKey, String title, String description, java.sql.Timestamp reminderDate, String phone, String email) {
 		// TODO we need to generate the id query max id and add 1 to get new ID
 		Integer stickyNoteRecord = stickyNoteDao.insertStickyNote(uniqueKey,title,description, reminderDate, phone, email);
 		log.info("Service: Inserted stickyNoteRecord."+stickyNoteRecord);
 	}
 
+	/**
+	 * Retrieve all sticky notes for unique key
+	 * @param uniqueKey
+	 * @return
+	 */
 	public List<StickyNote> retrieveStickyNote(String uniqueKey) {
 		return stickyNoteDao.retrieveStickyNotesByUniqueKey(uniqueKey);
 	}
 
+	/**
+	 * Delete a single sticky note by sticky note ID.
+	 * @param stickyNoteId
+	 */
     public void deleteStickyNote(String stickyNoteId) {
 		Integer stickyNoteRecord = stickyNoteDao.deleteStickyNote(stickyNoteId);
     	log.info("Service: Deleted stickyNoteRecord."+stickyNoteRecord);
